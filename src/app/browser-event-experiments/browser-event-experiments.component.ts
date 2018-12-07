81a810c1cf4826f31ab40912aa727cf3876362cd
Ryan Efendy <ryanefendy95@gmail.com>
2018-12-07 09:48:04 -0800

    FEAT: browser event experiments

---
+++ src/app/browser-event-experiments/browser-event-experiments.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'browser-event-experiments',
+  templateUrl: './browser-event-experiments.component.html',
+  styleUrls: ['./browser-event-experiments.component.css']
+})
+export class BrowserEventExperimentsComponent implements OnInit {
+  hoverSection: HTMLElement;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.hoverSection = document.getElementById('hover');
+    this.hoverSection.addEventListener('mousemove', event =>
+      console.log(event)
+    );
+  }
+}
